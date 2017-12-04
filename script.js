a6f0c6e101da53b21d43c4ee7c274072e150ba1e
CONLEYE <e.m.conley@lse.ac.uk>
2017-12-04 11:38:38 +0000

    Small edits to the Javascript
    > added z index to the active slide
    > changed java syntax

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 (function() {
+  const myQuestions = [
+   {
+      question: "What percent of students said they had a fear of failure? (YouGov, 2017)",
+      answers: {
+        a: "35%",
+        b: "52%",
+        c: "77%"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "How many students said that they feel levels of fear that interefere with their daily lives? (YouGov, 2017)",
+      answers: {
+        a: "5/10",
+        b: "6/10",
+        c: "7/10"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What percent of students suffer from stress or anxiety? (UniHealth, 2017)",
+      answers: {
+        a: "62%",
+        b: "72%",
+        c: "82%",
+      },
+      correctAnswer: "c"
+    }
+  ];
+
   function buildQuiz() {
     // we'll need a place to store the HTML output
     const output = [];
@@ -12,18 +42,20 @@
       for (letter in currentQuestion.answers) {
         // ...add an HTML radio button
         answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
+          <label>
+             <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+           </label>`
         );
       }
 
       // add this question and its answers to the output
       output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
+        `<div class="slide">
+           <div class="question"> ${currentQuestion.question} </div>
+           <div class="answers"> ${answers.join("")} </div>
+         </div>`
       );
     });
 
@@ -63,42 +95,50 @@
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
   }
 
+  function showSlide(n) {
+    slides[currentSlide].classList.remove("active-slide");
+    slides[n].classList.add("active-slide");
+    currentSlide = n;
+    
+    if (currentSlide === 0) {
+      previousButton.style.display = "none";
+    } else {
+      previousButton.style.display = "inline-block";
+    }
+    
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = "none";
+      submitButton.style.display = "inline-block";
+    } else {
+      nextButton.style.display = "inline-block";
+      submitButton.style.display = "none";
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
   const quizContainer = document.getElementById("quiz");
   const resultsContainer = document.getElementById("results");
   const submitButton = document.getElementById("submit");
-  const myQuestions = [
-    {
-      question: "What percent of students said they had a fear of failure? (YouGov, 2017)",
-      answers: {
-        a: "35%",
-        b: "52%",
-        c: "77%"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "How many students said that they feel levels of fear that interefere with their daily lives? (YouGov, 2017)",
-      answers: {
-        a: "5/10",
-        b: "6/10",
-        c: "7/10"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "What percent of students suffer from stress or anxiety? (UniHealth, 2017)",
-      answers: {
-        a: "62%",
-        b: "72%",
-        c: "82%",
-      },
-      correctAnswer: "c"
-    }
-  ];
 
   // display quiz right away
   buildQuiz();
 
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  showSlide(0);
+
   // on submit, show results
   submitButton.addEventListener("click", showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
 })();
